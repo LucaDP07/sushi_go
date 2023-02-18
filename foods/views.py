e4d0e957553dc8c694fafe2302284d432e170067
LucaDP07 <luke07@hotmail.it>
2023-02-18 12:00:19 +0000

    added foods views and templates

--- foods/views.py
+++ foods/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Food
 
 # Create your views here.
+
+def all_foods(request):
+    """ A view to show all foods, including sorting and search queries """
+
+    foods = Food.objects.all()
+
+    context = {
+        'foods': foods,
+    }
+
+    return render(request, 'foods/foods.html', context)
