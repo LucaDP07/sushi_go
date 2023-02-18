57dc8de3cff21385f33adbae94fc7477229fa340
LucaDP07 <luke07@hotmail.it>
2023-02-18 14:52:18 +0000

    added foods details functionality

--- foods/urls.py
+++ foods/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.all_foods, name='foods')
+    path('', views.all_foods, name='foods'),
+    path('<food_id>', views.food_detail, name='food_detail'),
 ]
--- foods/views.py
+++ foods/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Food
 
-# Create your views here.
 
 def all_foods(request):
     """ A view to show all foods, including sorting and search queries """
@@ -13,3 +12,14 @@ def all_foods(request):
     }
 
     return render(request, 'foods/foods.html', context)
+
+def food_detail(request, food_id):
+    """ A view to show individual food details """
+
+    food = get_object_or_404(Food, pk=food_id)
+
+    context = {
+        'food': food,
+    }
+
+    return render(request, 'foods/food_detail.html', context)
