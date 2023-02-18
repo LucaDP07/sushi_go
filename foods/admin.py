4bf8d584a963a4c22359c929917f23a89d877be0
LucaDP07 <luke07@hotmail.it>
2023-02-18 11:45:18 +0000

    customized admin

--- foods/admin.py
+++ foods/admin.py
@@ -1,6 +1,26 @@
 from django.contrib import admin
 from .models import Food, Category
 
-# Register your models here.
-admin.site.register(Food)
-admin.site.register(Category)
+
+class FoodAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'rating',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+admin.site.register(Food, FoodAdmin)
+admin.site.register(Category, CategoryAdmin)
