3d3b6b7e3398893153d3a9712e1509b0b5c6a768
LucaDP07 <luke07@hotmail.it>
2023-02-18 23:06:18 +0000

    added category filtering

--- foods/views.py
+++ foods/views.py
@@ -1,31 +1,40 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Food
+from .models import Food, Category
 
+# Create your views here.
 
 def all_foods(request):
     """ A view to show all foods, including sorting and search queries """
 
     foods = Food.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            foods = foods.filter(category__name__in=categories)
+            categories = Category.objects.filter(name__in=categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "No search criteria yet!")
+                messages.error(request, "Please enter your search criteria!")
                 return redirect(reverse('foods'))
-
-        queries = Q(name__icontains=query) | Q(description__icontains=query)
-        foods = foods.filter(queries)
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            foods = foods.filter(queries)
 
     context = {
         'foods': foods,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'foods/foods.html', context)
+
 
 def food_detail(request, food_id):
     """ A view to show individual food details """
