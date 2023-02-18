d7a042a989ccd727b5f498bbc2969fa892b81eeb
LucaDP07 <luke07@hotmail.it>
2023-02-18 15:54:17 +0000

    added search functionality

--- foods/views.py
+++ foods/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Food
 
 
@@ -6,9 +8,21 @@ def all_foods(request):
     """ A view to show all foods, including sorting and search queries """
 
     foods = Food.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No search criteria yet!")
+                return redirect(reverse('foods'))
+
+        queries = Q(name__icontains=query) | Q(description__icontains=query)
+        foods = foods.filter(queries)
 
     context = {
         'foods': foods,
+        'search_term': query,
     }
 
     return render(request, 'foods/foods.html', context)
