2a7da7fd658f12a9473d5a7166bef23238320ad7
LucaDP07 <luke07@hotmail.it>
2023-02-19 14:08:45 +0000

    updated basket template

--- basket/contexts.py
+++ basket/contexts.py
@@ -1,5 +1,7 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from foods.models import Food
 
 
 def basket_contents(request):
@@ -7,6 +9,17 @@ def basket_contents(request):
     basket_items = []
     total = 0
     food_count = 0
+    basket = request.session.get('basket', {})
+
+    for item_id, quantity in basket.items():
+        food = get_object_or_404(Food, pk=item_id)
+        total += quantity * food.price
+        food_count += quantity
+        basket_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'food': food,
+        })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -22,5 +22,4 @@ def add_to_basket(request, item_id):
         basket[item_id] = quantity
 
     request.session['basket'] = basket
-    print(request.session['basket'])
     return redirect(redirect_url)
