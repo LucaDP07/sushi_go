081911e030593bbdf8e3f9535eeab7cf1cacca1b
LucaDP07 <luke07@hotmail.it>
2023-02-20 19:39:51 +0000

    Added adjust basket and remove from basket views

--- basket/views.py
+++ basket/views.py
@@ -1,16 +1,16 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
 
 def view_basket(request):
-    """ A view that renders the basket contents page """
+    """ A view that renders the bag contents page """
 
     return render(request, 'basket/basket.html')
 
 
 def add_to_basket(request, item_id):
-    """ Add a quantity of the specified food to the basket """
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
@@ -18,8 +18,38 @@ def add_to_basket(request, item_id):
 
     if item_id in list(basket.keys()):
         basket[item_id] += quantity
+        messages.success(
+            request, f'Updated {food.name} quantity to {basket[item_id]}')
     else:
         basket[item_id] = quantity
 
     request.session['basket'] = basket
     return redirect(redirect_url)
+
+
+def adjust_basket(request, item_id):
+    """ Adjust quantity of the product in shopping bag """
+
+    quantity = int(request.POST.get('quantity'))
+    basket = request.session.get('basket', {})
+
+    if  quantity > 0:
+        basket[item_id] = quantity
+    else:
+        basket.pop(item_id)
+
+    request.session['basket'] = basket
+    return redirect(reverse('view_basket'))
+
+
+def remove_from_basket(request, item_id):
+    """ Remove the item from the shopping bag """
+    try:
+        basket = request.session.get('basket', {})
+        basket.pop(item_id)
+
+        request.session['basket'] = basket
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
