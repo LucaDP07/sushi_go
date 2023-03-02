4fb403fe02b38b18c4877e64d82f87b22a752f1b
LucaDP07 <luke07@hotmail.it>
2023-03-02 22:24:22 +0000

    added forms, admin and signals

--- order/apps.py
+++ order/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 
-class OrderConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
+class CheckoutConfig(AppConfig):
     name = 'order'
+
+    def ready(self):
+        import order.signals
