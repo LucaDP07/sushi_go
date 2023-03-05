1231e9eebb27af2e1623417c1a215d654b40194c
LucaDP07 <luke07@hotmail.it>
2023-03-05 21:18:28 +0000

    styled wishlist page and added urls and views

--- profiles/views.py
+++ profiles/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404
 from django.contrib import messages
-
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import UserProfile
 from .forms import UserProfileForm
-
+from foods.models import Food
 from order.models import Order
 
 
@@ -45,4 +46,36 @@ def order_history(request, order_number):
     }
 
     return render(request, template, context)
+
+
+# Very Academy code from YouTube tutorial
+@login_required
+def wishlist(request):
+    """ Display the users wish list page """
+    if request.user.is_authenticated:
+        # print(request.user.id)
+        foods = Food.objects.filter(users_wishlist=request.user)
+        # foods = Food.objects.all()
+        
+        return render(request, 'profiles/wishlist.html',
+                      {'wishlist': foods})
+    else:
+        messages.error(request, 'Sorry you must be \
+            signed in to use a Wish List.')
+        return redirect('home')
+
+
+# Very Academy code from YouTube tutorial
+@login_required
+def add_to_wishlist(request, id):
+    food = get_object_or_404(Product, id=id)
+    if product.users_wishlist.filter(id=request.user.id).exists():
+        product.users_wishlist.remove(request.user)
+        messages.success(request, product.name + ' has been \
+                         removed from your Wish List')
+    else:
+        product.users_wishlist.add(request.user)
+        messages.success(request, 'Added ' + product.name + ' to \
+                         your Wish List')
+    return HttpResponseRedirect(request.META["HTTP_REFERER"])
 
