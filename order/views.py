9ae61d89e179f2fa7421beda89caba88a906767c
LucaDP07 <luke07@hotmail.it>
2023-03-08 20:36:44 +0000

    Updated webhook handler

--- order/views.py
+++ order/views.py
@@ -48,7 +48,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_basket = json.dumps(bag)
+            order.save()
             for item_id, item_data in basket.items():
                 try:
                     food = Food.objects.get(id=item_id)
