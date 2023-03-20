b8efa2bfd997d312529968007d2527a82875ee9a
LucaDP07 <luke07@hotmail.it>
2023-03-20 14:09:52 +0000

    set debug to false for deployment

--- sushi_go/settings.py
+++ sushi_go/settings.py
@@ -14,9 +14,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = [["sushi-go.herokuapp.com", "localhost"]]
+ALLOWED_HOSTS = ["sushi-go.herokuapp.com", "localhost"]
 
 
 # Application definition
