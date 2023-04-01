6f8430fbdac6fab715677f507d608e6baf2ffe1e
LucaDP07 <luke07@hotmail.it>
2023-04-01 13:11:41 +0000

    changed food model field

--- foods/models.py
+++ foods/models.py
@@ -29,7 +29,7 @@ class Food(models.Model):
         max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    users_wishlist = models.ManyToManyField(User)
+    users_wishlist = models.ManyToManyField(User, blank=True)
 
     def __str__(self):
         return self.name
